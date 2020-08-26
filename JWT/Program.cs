6ad8b01d5fdd16295ad653b891a807fd8c0373de
Stefan Kubinski <stefan.kubinski@outlook.com>
2020-08-26 13:39:46 +0200

    Add project files.

---
+++ JWT/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Claims;
+using System.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace JWT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sign());
+            Console.ReadLine();
+        }
+
+        static string Sign()
+        {
+
+            string audience = "https://appleid.apple.com";
+            string issuer = ""; //team 
+            string subject = ""; //service
+            string p8key = ""; //key
+
+            IList<Claim> claims = new List<Claim> {
+                                new Claim ("sub", subject)
+            };
+
+            CngKey cngKey = CngKey.Import(Convert.FromBase64String(p8key), CngKeyBlobFormat.Pkcs8PrivateBlob);
+
+            SigningCredentials signingCred = new SigningCredentials(
+                new ECDsaSecurityKey(new ECDsaCng(cngKey)),
+                SecurityAlgorithms.EcdsaSha256
+            );
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer,
+                audience,
+                claims,
+                DateTime.Now,
+                DateTime.Now.AddDays(180),
+                signingCred
+            );
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+
+            string jwt = tokenHandler.WriteToken(token);
+            return jwt;
+        }
+    }
+}
